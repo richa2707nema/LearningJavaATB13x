ceb6a49474e7b9ad2b0762782757ce8c36be2a91
Richa Nema <richa.nema.contractor@pepsico.com>
2025-06-26 08:21:53 +0530

    New laearning

---
+++ src/Test26062025/BasicFunctiondemo.java
@@ -0,0 +1,14 @@
+package Test26062025;
+
+public class BasicFunctiondemo {
+    public static void main(String[] args) {
+        {
+            String s1 ="Hello World";
+            System.out.println(s1.length());
+            System.out.println(s1.concat(" I am Here"));
+            System.out.println(s1.substring(1,7));
+            System.out.println(s1.charAt(6));
+
+        }
+    }
+}
