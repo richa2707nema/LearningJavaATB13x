d39db2a2eb0eb458e402a21ea55e8c0c46bb9008
Richa Nema <richa.nema.contractor@pepsico.com>
2025-06-26 08:31:44 +0530

    New laearning

---
+++ src/Test26062025/Comparisionfunction.java
@@ -0,0 +1,16 @@
+package Test26062025;
+
+public class Comparisionfunction {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = new String("hello");
+        String s3 ="Hello";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s1.compareTo(s2));
+
+
+
+
+    }
+}
